d6e94801f8c06305bed777a41f542e27c4f0b2b6
Harmangulati <83772120+Harmangulati@users.noreply.github.com>
2021-12-02 14:53:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,25 @@ function preload(){
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+ console.log("Model Is Initialize");   
+}
+function gotPoses(results){
+if(results.length>0){
+console.log(results);
+console.log("nose x="+results[0].pose.nose.x);
+console.log("nose y="+results[0].pose.nose.y);
+}
 }
 function draw(){
-
+image(video,0,0,300,300);
+}
+function take_snapshot(){
+save('image.png');
 }
