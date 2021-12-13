127c1528eaf74082f7a49669ca4c227e87b2cc52
Harmangulati <83772120+Harmangulati@users.noreply.github.com>
2021-12-13 14:52:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX=0;
+noseY=0;
 function preload(){
-
+mustache= loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 function setup(){
     canvas=createCanvas(300,300);
@@ -16,12 +18,13 @@ function modelLoaded(){
 function gotPoses(results){
 if(results.length>0){
 console.log(results);
-console.log("nose x="+results[0].pose.nose.x);
-console.log("nose y="+results[0].pose.nose.y);
+noseX=results[0].pose.nose.x-40;
+noseY=results[0].pose.nose.y;
 }
 }
 function draw(){
 image(video,0,0,300,300);
+image(mustache,noseX,noseY,80,35);
 }
 function take_snapshot(){
 save('image.png');
